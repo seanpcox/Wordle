ab9799c41ee0aa8fb7bc4edea03285230be1ae63
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-07-10 23:31:40 +0100

    Intial project structure and first class

---
+++ Wordy/src/common/enum/LetterState.py
@@ -0,0 +1,19 @@
+# @author: seanpcox
+
+import enum
+
+class LetterState(enum.Enum):
+
+    def __new__(cls, *args, **kwds):
+        value = len(cls.__members__) + 1
+        obj = object.__new__(cls)
+        obj._value_ = value
+        return obj
+    
+    def __init__(self, color):
+        self.color = color
+
+    NOT_ASSIGNED = "white"
+    INCORRECT = "gray"
+    WRONG_POSITION = "yellow"
+    CORRECT = "green"
